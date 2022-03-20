65d3f38e295673d1bf6da849e8133e9a308fc616
mhmmdd <mhmmd.dd@gmail.com>
2022-03-20 21:37:27 +0300

    Snapshot

--- ProductService/src/main/java/com/example/api/ProductService/product/command/ProductAggregate.java
+++ ProductService/src/main/java/com/example/api/ProductService/product/command/ProductAggregate.java
@@ -15,7 +15,7 @@ import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 
-@Aggregate
+@Aggregate(snapshotTriggerDefinition = "productSnapshotTriggerDefinition")
 @NoArgsConstructor
 public class ProductAggregate {
 
